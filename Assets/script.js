d42149fa1e893e3fb568cca1bc0f3fc7de68b9bc
Tracy Angell <63623364+tracye1083@users.noreply.github.com>
2021-04-03 19:12:01 -0400

    updates to form and removal of jquery and high score functionality

--- Assets/script.js
+++ Assets/script.js
@@ -6,25 +6,39 @@ let playerScore = 0
 
 // questions
 let questions = [{
-        questionText: 'How many fluffy bunnies are there?',
+        questionText: 'Which of these is not a way to save a variable?',
         answers: [
-            '1 Bunny', '2 Bunnies', '3 Bunnies'
+            'var', 'vet', 'let', 'const'
         ],
         correctAnswer: 1
     },
     {
-        questionText: 'How many soft duckies are there?',
+        questionText: 'Which of the following is not considered a JavaScript operator?',
         answers: [
-            '1 Duckie', '2 Duckies', '3 Duckies'
+            'this', 'new', 'typeof', 'delete'
         ],
         correctAnswer: 0
     },
     {
-        questionText: 'How many cookies should I eat?',
+        questionText: '_______ is the process of finding errors and fixing them within a program.',
         answers: [
-            '1 cookie', '2 cookies', 'Never enough cookies'
+            'Compiling', 'Executing', 'Debugging', 'Scanning'
         ],
         correctAnswer: 2
+    },
+    {
+        questionText: 'Which is NOT a commonly used data type?',
+        answers: [
+            'Boolean', 'Float', 'String', 'Number'
+        ],
+        correctAnswer: 1
+    },
+    {
+        questionText: 'Using a(n) ___ statement is how you test for a specific condition.',
+        answers: [
+            'Select', 'For', 'While', 'If'
+        ],
+        correctAnswer: 3
     }
 ]
 
@@ -34,7 +48,7 @@ let tick = function() {
     currentTime--
     if (currentTime <= 0) {
         // quiz finished, show high score thingie and stop counter
-        renderHighScores();
+        endGame();
     }
     let timerBox = document.getElementById('timer-display');
     if (timerBox) {
@@ -45,7 +59,9 @@ let tick = function() {
 // starting game function
 
 function startGame(event) {
-    console.log('clicked')
+    console.log('clicked Start')
+    document.getElementById('start-btn').style.display = "none";
+    document.getElementById('instructions').style.display = "none";
     currentQuestionIndex = 0;
     renderCurrentQuestion();
     document.getElementById('questions').style.display = "block";
@@ -58,81 +74,76 @@ function startGame(event) {
 function answerClick(event) {
     let answerId = event.target.id;
     console.log('i clicked ' + answerId);
-    // provide feedback on answer
+    // provide feedback on answer, increase score for correct, decrease timer for incorrect
     let answerIndex = parseInt(answerId.substring(6)) - 1;
     console.log(answerIndex)
     let correctAnswerIndex = questions[currentQuestionIndex].correctAnswer;
     if (answerIndex === correctAnswerIndex) {
-        playerScore = playerScore + 1;
         document.getElementById('feedback').textContent = "Correct answer!";
+        playerScore = playerScore + 1;
     } else {
         document.getElementById('feedback').textContent = "Wrong answer! -5 seconds from timer";
         currentTime = currentTime - 5;
     }
-
     currentQuestionIndex++;
     if (currentQuestionIndex >= questions.length) {
         // we are at the end of the quiz
-        renderHighScores();
+        endGame();
     } else {
         // show the next question
         renderCurrentQuestion();
     }
-    // submitScore();
-    // renderHighScores();
 }
 
-// function renderSubmitScore() {
-//     submitScore;
-// }
-
-// function submitScore() {
-//     let name = document.getElementById("myText").value;
-//     console.log('it worked')
-//     let scores = JSON.parse(localStorage.getItem("scores")) || [];
-
-//     scores.push({
-//         name: name,
-//         score: timer
-//     })
-
-//     localStorage.setItem("scores", JSON.stringify(scores));
-//     renderHighScores;
-// }
-
-function renderHighScores() {
-
-    if (timer) {
-        clearInterval(timer);
-    }
-    document.getElementById('questions').style.display = "none";
-    document.getElementById('answers').style.display = "none";
-    document.getElementById('myText').style.display = "block";
-    document.getElementById('submitScore').style.display = "block";
-    document.getElementById('scores').textContent = "Highscore";
-    let name = document.getElementById("myText").value;
-    let scores = JSON.parse(localStorage.getItem("scores")) || [];
-
-    scores.push({
-        name: name,
-        score: playerScore
-    })
-
-    localStorage.setItem("scores", JSON.stringify(scores));
-
-    for (let i = 0; i < scores.length; i++) {
-        let score = scores[i]
-        $(".scores").append("<li><span>" + score.name + "</span> <span>" + score.score + "</span></li>")
-    }
-}
-
+// populates next question
 function renderCurrentQuestion() {
     let question = questions[currentQuestionIndex];
     document.getElementById('questions').textContent = question.questionText;
     document.getElementById('answer1').textContent = question.answers[0];
     document.getElementById('answer2').textContent = question.answers[1];
     document.getElementById('answer3').textContent = question.answers[2];
+    document.getElementById('answer4').textContent = question.answers[3];
 }
+
+function endGame() {
+    console.log('endGame called')
+    if (timer) {
+        clearInterval(timer);
+    }
+    // hides questions and answers display 
+    document.getElementById('questions').style.display = "none";
+    document.getElementById('answers').style.display = "none";
+    document.getElementById('feedback').style.display = "none";
+    // shows input to enter player initials and player scores
+    document.getElementById('myText').style.display = "block";
+    document.getElementById('submitScore').style.display = "block";
+}
+
+function submitYourScore() {
+    // get user submitted initials to submit to local storage 
+    let name = document.getElementById("myText").value;
+    let scores = JSON.parse(localStorage.getItem("scores")) || [];
+    console.log(scores)
+    scores.push({
+        name: name,
+        score: playerScore
+    })
+    console.log(scores)
+    localStorage.setItem("scores", JSON.stringify(scores));
+    renderHighScores(scores);
+}
+
+// Player enters Initials and submits score which is printed in the list of player scores
+function renderHighScores(scoresToRender) {
+    console.log('render high scores called')
+
+    for (let i = 0; i < scoresToRender.length; i++) {
+        let thisScore = scoresToRender[i]
+        document.getElementById('scores').append("<li><span>" + thisScore.name + "</span> <span>" + thisScore.score + "</span></li>")
+    }
+    document.getElementById('scores').textContent = "Player Scores";
+}
+
 
 document.getElementById('start-btn').addEventListener("click", startGame);
 let answerButtons = document.querySelectorAll('.answer-button');
@@ -140,4 +151,4 @@ for (let i = 0; i < answerButtons.length; i++) {
     answerButtons[i].addEventListener("click", answerClick);
 }
 
-document.getElementById('submitScore').addEventListener("click", renderHighScores);
+document.getElementById('submitScore').addEventListener("click", submitYourScore);
